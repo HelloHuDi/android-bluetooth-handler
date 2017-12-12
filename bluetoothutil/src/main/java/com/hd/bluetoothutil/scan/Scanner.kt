b463f50b94113fc70637492b4767963a4a719442
hudi <hellohudi@163.com>
2017-12-12 17:33:00 +0800

    perfect scanner

--- bluetoothutil/src/main/java/com/hd/bluetoothutil/scan/Scanner.kt
+++ bluetoothutil/src/main/java/com/hd/bluetoothutil/scan/Scanner.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import com.hd.bluetoothutil.callback.ScannerCallback
 import com.hd.bluetoothutil.config.BluetoothDeviceEntity
 import com.hd.bluetoothutil.config.DeviceVersion
+import com.hd.bluetoothutil.help.BluetoothSecurityCheck
 
 
 /**
@@ -14,14 +15,19 @@ import com.hd.bluetoothutil.config.DeviceVersion
 object Scanner {
 
     /** start scan bluetooth device*/
-    fun scan(context: Context, bluetoothAdapter: BluetoothAdapter, entity: BluetoothDeviceEntity, callback: ScannerCallback) {
-        val scan = if (entity.version == DeviceVersion.BLUETOOTH_2) {
-            Bluetooth2Scanner()
+    fun scan(context: Context, bluetoothAdapter: BluetoothAdapter?=null, entity: BluetoothDeviceEntity, callback: ScannerCallback) {
+        val adapter = bluetoothAdapter ?: BluetoothSecurityCheck.newInstance(context).check(entity.version)
+        if (adapter == null) {
+            callback.scan(true)
         } else {
-            Bluetooth4Scanner()
+            val scan = if (entity.version == DeviceVersion.BLUETOOTH_2) {
+                Bluetooth2Scanner()
+            } else {
+                Bluetooth4Scanner()
+            }
+            scan.init(context, adapter, entity, callback)
+            scan.startScan()
         }
-        scan.init(context,bluetoothAdapter, entity, callback)
-        scan.startScan()
     }
 
 }
